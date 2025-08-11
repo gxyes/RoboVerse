b6f427977aae6fb39201a3c49fec819cb683bf43
weisonweileen <weisonweileen@gmail.com>
2025-08-10 20:10:27 -0700

    [update] get_started_0 past
    Signed-off-by: weisonweileen <weisonweileen@gmail.com>

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 d = dict()
-print(d.values())              # dict_values([]) 空的
+print(d.values())  # dict_values([]) 空的
 
 d["hip"] = "a"
 d["knee"] = "b"
 
-print(list(d.values()))        # [actuator1, actuator2]
+print(list(d.values()))  # [actuator1, actuator2]
 
 # for a in d.values():           # 遍历“值”
 #     a.reset()
