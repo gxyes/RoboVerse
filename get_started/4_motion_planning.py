cde80c3c216aacb922b153bed03c6c52ade82869
Guo Xinying <gxynoz@gmail.com>
2025-08-12 08:24:31 +0800

    [add] get_started 4&7&9
    * debug and merge and add examples
    
    * clean format

--- get_started/4_motion_planning.py
+++ get_started/4_motion_planning.py
@@ -38,7 +38,7 @@ class Args:
     robot: str = "franka"
 
     ## Handlers
-    sim: Literal["isaaclab", "isaacgym", "genesis", "pybullet", "sapien2", "sapien3", "mujoco"] = "mujoco"
+    sim: Literal["isaaclab", "isaacsim", "isaacgym", "genesis", "pybullet", "sapien2", "sapien3", "mujoco"] = "mujoco"
 
     ## Others
     num_envs: int = 1
